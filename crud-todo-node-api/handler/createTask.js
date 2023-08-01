d7dae1dc3da0d375914a4f7237cb5e6d79a888c9
Lizz1102 <lizz05110@gmail.com>
2023-08-01 09:51:46 -0400

    Initial commit: Create task function

---
+++ crud-todo-node-api/handler/createTask.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+
+const TASK_TABLE = process.env.TASK_TABLE;
+const dynamoDB = new AWS.dynamoDB.DocumentClient();
+const uuid = require("uuid");
+
+exports.createTask = async (event, context) => {
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        TableName: TASK_TABLE,
+        Item: {
+            id: uuid.v1(),
+            task: data.task,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp,
+        },
+    };
+
+    if (typeof data.task !== "string") {
+        console.error("Validation Failed");
+        return;
+    }
+
+    try {
+        body = await dynamoDb.put((params)).promise();
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+        console.log(err);
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers
+    };
+};
